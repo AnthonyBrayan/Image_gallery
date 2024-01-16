706f3bf901fc701fa7f941d5a1e563795e6aff6e
Anthony Brayan <brayan.farronan@gmail.com>
2024-01-16 14:03:56 +0100

    logo en carpeta puclic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//React
+
+import { useEffect } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const loginUser =async () => {
+    try {
+      const response= await fetch('http://127.0.0.1:5000/auth/',{
+        method: "POST",
+        headers:{
+          Accept:"application/json",
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+          username:"AFARRONAN",
+          password:"123456"
+        })
+      });
+            //Sacar el cuerpo de la respuesta (Body)
+            const data= await response.json();
+            console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  };
+
+  useEffect(() => {
+    loginUser();
+    console.log('Anthony Brayan');
+  }, []);
+
+  return <h1>Anthony</h1>
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
+//React
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+//Components
 import App from './App.jsx'
+
+//Styles
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
